9478ef111ed11b21df86960f98e6d5c29498c4ef
Andy Fiedler <andyfidd@gmail.com>
2019-07-02 15:30:28 -0500

    add lab 2

---
+++ aboutMe/app.js
@@ -0,0 +1,50 @@
+var correctCount = 0;
+
+//Question 1
+var answer1 = prompt('Fiedler prefers his first name pronounced Andy. True/False, (T/F)');
+var sanitizedAnswer1 = answer.toUpperCase().trim();
+if (sanitizedAnswer1 === 'TRUE'|| sanitizedAnswer1 ==='T') {
+    alert('Yes. Dont get cocky, kid. Only the first question.');
+        else if (sanitizedAnswer1 === 'F' || sanitizedAnswer1 ==='FALSE') {
+        else {
+         alert('What was that!? Maybe just give up now.');
+
+    //Question 2
+    var answer2 = prompt('Fiedler is an easily pronounced name. True/False (T/F)');
+    var sanitizedAnswer2 = answer2.toUpperCase().trim()
+    if (sanitizedAnswer2 === 'TRUE'|| sanitizedAnswer2 ==='T') {
+        alert('Yes. Whatever. Good job.');
+        else if (sanitizedAnswer2 === 'F' || sanitizedAnswer2 === "FALSE") {
+        else {
+            alert('Yeahhhhh, no.');
+        }
+
+        //Question 3
+        var answer3 = prompt('Fiedler is not a huge fan of his doggo. (T/F)');
+        var sanitizedAnswer3 = answer3.toUpperCase().trim()
+            if (sanitizedAnswer3 === 'TRUE'|| sanitizedAnswer3 ==='T') {
+                alert('Ok. Whatever. Good job.');
+                else if (sanitizedAnswer3 === 'F' || sanitizedAnswer3 === "FALSE") {
+                else {
+                    alert('Yeahhhhh, no.');
+                }
+
+            //Question 4
+            var answer4 = prompt('Fiedler is a people person. (T/F)');
+            var sanitizedAnswer4 = answer4.toUpperCase().trim()
+                if (sanitizedAnswer4 === 'TRUE'|| sanitizedAnswer4 ==='T') {
+                    alert('Ok. Whatever. Good job.');
+                    else if (sanitizedAnswer4 === 'F' || sanitizedAnswer4 === "FALSE") {
+                    else {
+                        alert('Not so much');
+                }
+
+            //Question 5
+            var answer5 = prompt('Fiedler is a people person. (T/F)');
+            var sanitizedAnswer5 = answer5.toUpperCase().trim()
+                if (sanitizedAnswer5 === 'TRUE'|| sanitizedAnswer5 ==='T') {
+                    alert('Neat.');
+                    else if (sanitizedAnswer5 === 'F' || sanitizedAnswer5 === "FALSE") {
+                    else {
+                        alert('Nah. Pay attention next time.');
+                    }
